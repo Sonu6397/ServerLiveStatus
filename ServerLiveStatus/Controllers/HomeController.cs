58944f4d59b6204dd36bce96259e759c35fc637a
chhavi raj singh <chhavirajsingh2000@gmail.com>
2022-12-04 08:55:18 +0530

    Add project files.

---
+++ ServerLiveStatus/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using ServerLiveStatus.dbo_connt;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ServerLiveStatus.Controllers
+{
+    public class HomeController : Controller
+    {
+        PesDataEntities4 db = new PesDataEntities4();
+        public ActionResult Index()
+        {
+            var res = db.ServersMonetories.ToList();
+            Response.AddHeader("Refresh", "5");
+            return View(res);
+          
+        }
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ dbo connt/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ServerLiveStatus.MvcApplication" Language="C#" %>
